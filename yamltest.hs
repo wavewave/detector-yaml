4ab0a0f5cc387cdb2205a9a82688b211f2d28a54
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-24 21:42:01 +0100

    yayaml to yamltest

--- yamltest.hs
+++ yamltest.hs
@@ -1,27 +1,82 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
-import GHC.Generics
+-- import           Blaze.ByteString.Builder
+-- import           Blaze.ByteString.Builder.Char8
+import           Data.Text.Lazy.Builder 
+import           Data.Attoparsec
+import qualified Data.ByteString.Lazy.Char8 as L
+-- import           Data.Foldable
+-- import qualified Data.HashMap.Strict as HM
+import           Data.List (intersperse)
+import           Data.Monoid ((<>), mconcat, mempty)
+import           Data.Scientific
+import qualified Data.String as S (IsString(..), fromString)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+import qualified Data.Vector as V
 
-import qualified Data.Yaml as Y
-import qualified Data.ByteString.Char8 as B
+data ListStyle = Inline | Wrapped 
 
-phoLowPtBins :: Y.Value
-phoLowPtBins = (Y.array . map Y.Number) [ 15.0, 18.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 60.0, 80.0, 100.0 ]  
+data YamlValue = YObject [(T.Text, YamlValue)]
+               | YLArray ListStyle [YamlPrimValue]
+               --  | YIArray [YamlValue]
+               | YPrim YamlPrimValue 
+
+data YamlPrimValue = YNumber Scientific
+                   | YString T.Text
+                   | YBool Bool 
+                   | YNull 
+
+instance S.IsString YamlPrimValue where
+  fromString str = YString (T.pack str) 
+
+instance S.IsString YamlValue where
+  fromString = YPrim . S.fromString
+
+defIndent :: Int
+defIndent = 4 
+
+buildYaml :: Int -> YamlValue -> Builder 
+buildYaml n (YObject m) = (mconcat . map (buildPair n) ) m
+buildYaml n (YLArray sty xs) = buildList sty n xs 
+buildYaml n (YPrim p) = buildPrim p 
+
+buildList :: ListStyle -> Int -> [YamlPrimValue] -> Builder 
+buildList Inline n xs = fromLazyText "[ "
+                        <> (mconcat . intersperse (fromLazyText ", ") . map buildPrim ) xs 
+                        <> fromLazyText " ]"
+buildList Wrapped n xs = fromLazyText "[ " 
+                         <> ( mconcat 
+                            . intersperse (makeIndent n <> fromLazyText ", ") 
+                            . map (\x -> buildPrim x <> fromLazyText "\n") ) xs
+                         <> makeIndent n <> fromLazyText "] "
+
+buildPrim (YNumber s) = scientificBuilder s 
+buildPrim (YString txt) = fromLazyText txt
+buildPrim (YBool b) = (fromLazyText . T.pack . show) b 
+buildPrim YNull = mempty 
+
+
+
+buildPair :: Int -> (T.Text, YamlValue) -> Builder
+buildPair n (k,v) = fromLazyText "\n" <> makeIndent n 
+                    <> fromLazyText k <> fromLazyText ": " <> buildYaml (n+defIndent) v 
+
+makeIndent :: Int -> Builder 
+makeIndent n = mconcat (replicate n (fromString "_"))
+
+testvalue = YObject $ [ ("abc", "haha")
+                      , ("def", testvalue2)
+                      , ("acd", testvalue3) 
+                      , ("lo" , YPrim (YNumber (fromFloatDigits 314.159)))
+                      ]
+testvalue2 = YObject $ [ ("hello", "okay") 
+                       , ("world", "no")
+                       ] 
+
+testvalue3 = YLArray Wrapped [ "a", "bc" , "abc", "babo" ] 
 
 main :: IO ()
 main = do 
-  let testobj = Y.object [ ("ElePtBins", Y.String "test" )  
-                         , ("EleEtaBins", Y.Number 10.0)
-                         , ("nEleeta", Y.String "test")
-                         , ("nElept", Y.Number 12)
-                         , ("TightEleEff", Y.String "test")
-                         , ("MediumEleEff", Y.String "test")
-                         , ("LooseEleEff", Y.String "test")
-                         , ("PhoLowPtBins", phoLowPtBins )
-                         , ("PhoHighPtBins", Y.String "test")
-                         , ("PhoEtaBins", Y.String "test")
-                         ] 
-
-  B.putStrLn $ Y.encode testobj 
-  
+  putStrLn "yayaml"
+  TIO.putStrLn $ toLazyText (buildYaml 4 testvalue)
