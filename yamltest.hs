5b93571add69f14b51915e30dbaed4d273b0ef31
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-25 08:43:28 +0000

    better formatting with list

--- yamltest.hs
+++ yamltest.hs
@@ -18,7 +18,7 @@ import qualified Data.Vector as V
 data ListStyle = Inline | Wrapped 
 
 data YamlValue = YObject [(T.Text, YamlValue)]
-               | YLArray ListStyle [YamlPrimValue]
+               | YLArray ListStyle [YamlValue]
                --  | YIArray [YamlValue]
                | YPrim YamlPrimValue 
 
@@ -26,6 +26,10 @@ data YamlPrimValue = YNumber Scientific
                    | YString T.Text
                    | YBool Bool 
                    | YNull 
+
+isPrim :: YamlValue -> Bool 
+isPrim (YPrim _) = True
+isPrim _ = False 
 
 instance S.IsString YamlPrimValue where
   fromString str = YString (T.pack str) 
@@ -41,14 +45,15 @@ buildYaml n (YObject m) = (mconcat . map (buildPair n) ) m
 buildYaml n (YLArray sty xs) = buildList sty n xs 
 buildYaml n (YPrim p) = buildPrim p 
 
-buildList :: ListStyle -> Int -> [YamlPrimValue] -> Builder 
+buildList :: ListStyle -> Int -> [YamlValue] -> Builder 
 buildList Inline n xs = fromLazyText "[ "
-                        <> (mconcat . intersperse (fromLazyText ", ") . map buildPrim ) xs 
+                        <> (mconcat . intersperse (fromLazyText ", ") . map (buildYaml n)) xs 
                         <> fromLazyText " ]"
-buildList Wrapped n xs = fromLazyText "[ " 
+buildList Wrapped n xs = fromLazyText "\n" <> makeIndent n 
+                         <> fromLazyText "[ " 
                          <> ( mconcat 
                             . intersperse (makeIndent n <> fromLazyText ", ") 
-                            . map (\x -> buildPrim x <> fromLazyText "\n") ) xs
+                            . map (\x -> buildYaml n x <> fromLazyText "\n") ) xs
                          <> makeIndent n <> fromLazyText "] "
 
 buildPrim (YNumber s) = scientificBuilder s 
@@ -60,7 +65,9 @@ buildPrim YNull = mempty
 
 buildPair :: Int -> (T.Text, YamlValue) -> Builder
 buildPair n (k,v) = fromLazyText "\n" <> makeIndent n 
-                    <> fromLazyText k <> fromLazyText ": " <> buildYaml (n+defIndent) v 
+                    <> fromLazyText k 
+                    <> fromLazyText ": "
+                    <> buildYaml (n+defIndent) v
 
 makeIndent :: Int -> Builder 
 makeIndent n = mconcat (replicate n (fromString "_"))
@@ -74,7 +81,11 @@ testvalue2 = YObject $ [ ("hello", "okay")
                        , ("world", "no")
                        ] 
 
-testvalue3 = YLArray Wrapped [ "a", "bc" , "abc", "babo" ] 
+testvalue3 = YLArray Wrapped [ v1, v2 , "abc", "babo" ] 
+
+v1 = YLArray Inline $ map (YPrim . YNumber) [1, 2, 3 ] 
+
+v2 = YLArray Inline $ map (YPrim . YNumber) [ 4, 5, 6 ] 
 
 main :: IO ()
 main = do 
