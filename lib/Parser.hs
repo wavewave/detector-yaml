52666929c86ec7a39e086e9f8f2817fa86ddf902
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-15 14:17:51 +0000

    rename to mkyaml. separate exe and lib and start parseyaml.hs

---
+++ lib/Parser.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Parser where 
+
+-- import Data.Attoparsec 
+-- import qualified Data.Text as T
+--
+-- import Detector 
+
+-- line :: Parser String
+-- line = takeTill 
+
+test :: IO ()
+test = putStrLn "test"
