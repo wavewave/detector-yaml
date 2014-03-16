dabd3e408fc209fb8bde5df7ba3f27fd4e8d77ae
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-16 20:45:12 +0000

    correct treatment of itemed list

--- lib/Parser.hs
+++ lib/Parser.hs
@@ -12,46 +12,57 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 --
 import Prelude hiding (takeWhile,dropWhile)
--- import Debug.Trace
+import Debug.Trace
 
+-- | parsed yaml 
 data PYaml = PYObject [ (T.Text, PYaml) ]
            | PYText T.Text
            | PYList [T.Text]
            deriving (Show, Eq)
 
+-- | generic text parser with designated delimiters
 p_text :: [Char] -> Parser T.Text
 p_text delim = 
     T.strip <$> takeTill (\x -> x `elem` delim) 
 
+-- | detect indentation
 p_indent :: Parser Int 
 p_indent = T.length <$> takeWhile (== ' ') 
 
-
+-- | comment parser
 p_comment :: Parser T.Text
 p_comment = char '#' *> takeTill (== '\n') <* char '\n'
 
+-- | combinator for getting rid of comment out of a parser
 p_commentOut :: (Monoid a) => Parser a -> Parser a
 p_commentOut p = mconcat <$> (p `sepBy` p_comment) 
 
+-- | no comment text
 p_textNoComment :: [Char] -> Parser T.Text
 p_textNoComment delim = 
     T.strip <$> p_commentOut content
   where content = takeTill (\x -> x `elem` '#':delim)
         
+
+-- | convenient function for sepBy with indentation and comment
+p_sepBy1CommentAndIndent :: Int -> Parser a -> Parser [a]
+p_sepBy1CommentAndIndent n p = p `sepBy1` p_sep
+  where spaces x = replicateM x (char ' ')
+        p_sep = takeTill (/= ' ')>> p_linebreaker >> spaces n
+
+-- | parser for a list with [ ] flow mode
 p_list :: Parser a -> Parser [a]
 p_list p = char '[' 
-           *> (p `sepBy` (char ','))
+           *> (p `sepBy1` (char ','))
            <* char ']'
 
+-- | list with flow with indentation and - 
 p_itemlist :: Int -> Parser a -> Parser [a]
-p_itemlist n p = many line
-  where line = replicateM n (char ' ') 
-               *> char '-'
-               *> p  
-               <* p_linebreaker
--- ((p_comment *> return ()) 
---                    <|> (char '\n' *> return ()))
+p_itemlist n p = p_sepBy1CommentAndIndent n line
+  where line = char '-' *> skipSpace *> p  
 
+
+-- | indentation-aware key value pair parser
 p_keyvalue :: (Int -> Parser b) 
            -> Parser (T.Text,b)
 p_keyvalue pv = do 
@@ -63,42 +74,41 @@ p_keyvalue pv = do
                in pv (n1 + n2))
     return (k,v)
 
+-- | parse key text. First output is the total length of text 
+-- consumed before ':'.  
 p_key :: Parser (Int,T.Text)
 p_key = do
-    c1 <- notChar ' ' 
+    c1 <- satisfy (notInClass [ ' ', '#', '\n', '-' ])
     txt' <- takeTill (`elem` [':','\n','#'])
     let txt = c1 `T.cons` txt' 
-    return (T.length txt, T.strip txt) 
-
+    trace (show txt) $ 
+      return (T.length txt, T.strip txt) 
 
 -- | primary text
 p_ptext :: Parser T.Text
 p_ptext = do 
-    c1 <- notChar ' ' 
+    c1 <- satisfy (notInClass [ ' ', '#', '\n', '-' ]) 
     txt' <- takeTill (`elem` [':','#','\n',','] )
     let txt = c1 `T.cons` txt' 
     return (T.strip txt) 
 
-
+-- | line breaker (either a simple newline or comment)
 p_linebreaker :: Parser ()
 p_linebreaker = (p_comment >> return ()) 
                 <|> (char '\n' >> return ())
 
+-- | generic parsed yaml object
 p_object :: Int -> Parser PYaml 
 p_object n = do 
     try (PYList <$> p_list (p_text [',',']']))
-    <|> do 
-      kvlst <- content `sepBy` 
-                 (takeTill (/= ' ')>> p_linebreaker >> spaces n)
-      if (not.null) kvlst 
-        then return (PYObject kvlst) 
-        else PYText <$> p_ptext
+    <|> try (PYList <$> p_itemlist n (p_text ['\n','#']))
+    <|> try (do kvlst <- p_sepBy1CommentAndIndent n content 
+                return (PYObject kvlst))
+    <|> PYText <$> p_ptext
   where 
     content = p_keyvalue p_object
-    spaces x = replicateM x (char ' ')
 
-
-
+-- | testing with a given file
 test :: FilePath -> IO ()
 test fp = do    
     txt <- TIO.readFile fp 
