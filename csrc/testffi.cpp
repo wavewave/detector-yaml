ba8d83f11b3cd9a9b9da71212aa731b35e5e09a8
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-18 22:30:15 +0000

    first yaml-cpp linked test program

--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "yaml-cpp/yaml.h"
+
+void yamltest( void ) {
+    YAML::Emitter out;
+    out << "Hello, World!";
+   
+    std::cout << "Here's the output YAML:\n" << out.c_str(); // prints "Hello, World!"
+}
+
 
 using namespace std;
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,6 +19,7 @@ extern "C" {
 void testffi( void )
 {
   cout << "test c++ ffi" << endl;
+  yamltest();
 }
 
 #ifdef __cplusplus
