06bb9528d99dd9cbbf2371c6ba99d594503641f6
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-19 01:40:22 +0000

    successful nested object parsing

--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -25,6 +25,19 @@ boost::optional<T> maybeFind( string key, YAML::Node doc ) {
   else return boost::optional<T>();
 }
 
+template<>
+boost::optional<YAML::Node> maybeFind( string key, YAML::Node doc ) {
+  if( doc[key] ) {
+    return boost::optional<YAML::Node>(doc[key]);
+  }
+  else return boost::optional<YAML::Node>();
+}
+
+struct object_description
+{ 
+  string name;
+};
+
 struct detector_description 
 { 
   string name; 
@@ -32,7 +45,22 @@ struct detector_description
   string reference; 
   string comment;
   string validation_info;
+  object_description object;
 }; 
+
+boost::optional<object_description> getObjectDescription( YAML::Node node)
+{
+  object_description od; 
+
+  boost::optional<string> s1 = maybeFind<string>("Name", node) ; 
+  if ( !s1.is_initialized() ) return boost::optional<object_description>();
+
+  od.name = *(s1.get_ptr());
+
+  return boost::optional<object_description>(od);
+} 
+
+
 
 boost::optional<detector_description> getDetectorDescription( YAML::Node doc ) 
 {
@@ -53,19 +81,27 @@ boost::optional<detector_description> getDetectorDescription( YAML::Node doc )
   boost::optional<string> s5 = maybeFind<string>("ValidationInfo", doc) ; 
   if ( !s5.is_initialized() ) return boost::optional<detector_description>();
 
+  boost::optional<YAML::Node> s6 = maybeFind<YAML::Node>("Object", doc) ; 
+  if ( !s6.is_initialized() ) return boost::optional<detector_description>();
+
   dd.name = *(s1.get_ptr());
   dd.description = *(s2.get_ptr());
   dd.reference = *(s3.get_ptr()); 
   dd.comment = *(s4.get_ptr());
   dd.validation_info = *(s5.get_ptr());
 
+  YAML::Node n1 = *(s6.get_ptr());
+  boost::optional<object_description> mo1 = getObjectDescription(n1); 
+  if( !mo1.is_initialized() ) return boost::optional<detector_description>();
+
+  dd.object = *(mo1.get_ptr());
 
   return boost::optional<detector_description>(dd);
-
 }
 
+
 void yamlparsetest( void ) {
-  std::ifstream input("temp/ATLAS2011.yaml");  
+  std::ifstream input("ATLAS2011.yaml");  
   YAML::Node doc = YAML::Load(input);
 
 
@@ -77,7 +113,7 @@ void yamlparsetest( void ) {
   cout << "Name is " << pdd->name << endl; 
   cout << "Description is "<< pdd->description << endl;
   cout << "Reference is " << pdd -> reference << endl;
-
+  cout << "Object.Name is " << pdd->object.name << endl;
 
   
 }
