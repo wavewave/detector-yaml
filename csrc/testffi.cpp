3bcecc4219157106b7e2000a6defc5875b37b366
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-22 14:05:05 +0000

    successful parsing either monad for import.

--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -30,10 +30,6 @@ boost::optional<YAML::Node> maybeFind( string key, YAML::Node doc ) {
   else return boost::optional<YAML::Node>();
 }
 
-struct import
-{
-  string name;
-};
 
 template<typename A, typename B>
 class either 
@@ -65,9 +61,25 @@ public:
   }
 };
 
-struct object_description
+struct import
+{
+  string name;
+};
+
+struct electron_eff_data 
 { 
   string name;
+}; 
+
+struct muon_eff_data
+{
+  string name;
+};
+
+struct object_description
+{ 
+  either <import,electron_eff_data> electron;
+  either <import,muon_eff_data> muon;
 };
 
 struct detector_description 
@@ -80,15 +92,73 @@ struct detector_description
   object_description object;
 }; 
 
+boost::optional< either<import,electron_eff_data> >
+getElectronEffData( YAML::Node node ) 
+{
+  either<import, electron_eff_data> r; 
+  
+  boost::optional<string> e1; 
+  boost::optional<string> s1; 
+  if( e1 = maybeFind<string>("Import", node) )
+  {
+    import imp; 
+    imp.name = e1.get();
+    r.put(imp);
+    return r;
+  }
+  else if( s1 = maybeFind<string>("Name", node) )
+  { 
+    electron_eff_data eeff; 
+    eeff.name = s1.get(); 
+    r.put(eeff);
+    return r; 
+  }
+  return NULL;
+}
+
+boost::optional< either<import,muon_eff_data> >
+getMuonEffData( YAML::Node node ) 
+{
+  either<import, muon_eff_data> r; 
+  
+  boost::optional<string> e1; 
+  boost::optional<string> s1; 
+  if( e1 = maybeFind<string>("Import", node) )
+  {
+    import imp; 
+    imp.name = e1.get();
+    r.put(imp);
+    return r;
+  }
+  else if( s1 = maybeFind<string>("Name", node) )
+  { 
+    muon_eff_data mueff; 
+    mueff.name = s1.get(); 
+    r.put(mueff);
+    return r; 
+  }
+  return NULL;
+}
+
+
 boost::optional<object_description> getObjectDescription( YAML::Node node)
 {
   object_description od; 
 
-  boost::optional<string> s1;
-  if ( s1 = maybeFind<string>("Name", node) ) 
-  {
-    od.name = s1.get();
+  boost::optional<YAML::Node> n1;
+  boost::optional<YAML::Node> n2;
+  boost::optional<either <import, electron_eff_data > > r1; 
+  boost::optional<either <import, muon_eff_data > > r2; 
 
+    
+  if ( (n1 = maybeFind<YAML::Node>("Electron", node) )
+       && (n2 = maybeFind<YAML::Node>("Muon", node) )
+       && (r1 = getElectronEffData(n1.get())) 
+       && (r2 = getMuonEffData(n2.get()))
+     )
+  {
+    od.electron = r1.get();
+    od.muon = r2.get();
     return boost::optional<object_description>(od);
   }
   
@@ -128,7 +198,7 @@ boost::optional<detector_description> getDetectorDescription( YAML::Node doc )
 
 
 void yamlparsetest( void ) {
-  std::ifstream input("ATLAS2011.yaml");  
+  std::ifstream input("temp/ATLAS2011.yaml");  
   YAML::Node doc = YAML::Load(input);
 
 
@@ -140,21 +210,21 @@ void yamlparsetest( void ) {
   cout << "Name is " << pdd->name << endl; 
   cout << "Description is "<< pdd->description << endl;
   cout << "Reference is " << pdd -> reference << endl;
-  cout << "Object.Name is " << pdd->object.name << endl;
-
-  either<int,string> t ;
-  // t.put( "hello" );
-  t.put( 32 );
-  boost::optional<int> tl = t.getLeft();   
-
-  if( tl.is_initialized() ) {
-    cout << "tl : " << tl.get() << endl; 
-  } 
-  else {
-    cout << "tl is not initialized" << endl;
+  either<import,electron_eff_data> e = pdd->object.electron; 
+  if( boost::optional<import> t1 = e.getLeft() )
+  { 
+    cout << "Object.Electron.Import is " << t1.get().name << endl;
   }
+  else cout << "Object.Electron parse failed" << endl;
+    
 
-  //<< ":" << t.isRight() << endl;  
+  either<import,muon_eff_data> mu = pdd->object.muon; 
+  if( boost::optional<muon_eff_data> t2 = mu.getRight() )
+  { 
+    cout << "Object.Muon.Eff_data is " << t2.get().name << endl;
+  }
+  else cout << "Object.Muon parse failed" << endl;
+
 }
 
 
