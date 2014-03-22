85bde02e48f76f4a0d73597fe48ee7c480b84a10
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-22 15:10:43 +0000

    separate type into detector/type.h

---
+++ csrc/detector/type.h
@@ -0,0 +1,119 @@
+#ifndef __DETECTOR_TYPE__
+#define __DETECTOR_TYPE__ 
+
+#include <boost/optional/optional.hpp>
+#include <boost/variant.hpp>
+
+using namespace std; 
+
+
+template<typename A, typename B>
+class either 
+{
+private: 
+  enum which_t { left, right } which;
+  boost::variant<A,B> content; 
+public: 
+  void put(A a) { which = left; content = a; } 
+  void put(B b) { which = right; content = b; }
+
+  bool isLeft( void ) { return (which == left); }
+  bool isRight( void ) { return (which == right); }
+
+  boost::optional<A> getLeft ( void ) 
+  {
+    if( which == left ) {
+      return boost::optional<A>(boost::get<A>(content)); 
+    }
+    else return boost::optional<A>(); 
+  }
+  boost::optional<B> getRight( void )
+  {
+    if( which == right ) 
+    {
+      return boost::optional<B>(boost::get<B>(content));
+    }
+    else return boost::optional<B>();
+  }
+};
+
+
+struct import
+{
+  string name;
+};
+
+struct meta_info_t
+{
+  string tag;
+  string description;
+  string comment; 
+  string reference; 
+};
+
+struct electron_eff_data_t
+{ 
+  string name;
+  meta_info_t meta_info; 
+}; 
+
+struct photon_eff_data_t
+{ 
+  string name;
+  meta_info_t meta_info; 
+}; 
+
+struct bjet_eff_data_t
+{ 
+  string name;
+  meta_info_t meta_info; 
+}; 
+
+struct muon_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+
+struct jet_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+
+struct tau_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+
+/*
+struct pt_threshold_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+*/
+
+struct object_description_t
+{ 
+  either <import,electron_eff_data_t> electron;
+  either <import,photon_eff_data_t> photon;
+  either <import,bjet_eff_data_t> bjet;
+  either <import,muon_eff_data_t> muon;
+  either <import,jet_eff_data_t> jet;
+  either <import,tau_eff_data_t> tau;
+};
+
+struct detector_description_t
+{ 
+  string name; 
+  string description; 
+  string reference; 
+  string comment;
+  string validation_info;
+  object_description_t object;
+}; 
+
+#endif // __DETECTOR_TYPE__
+
--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 #include <boost/optional/optional.hpp>
 #include <boost/variant.hpp>
-#include "yaml-cpp/yaml.h"
+#include <yaml-cpp/yaml.h>
+// 
+#include <detector/type.h>
+
 
 using namespace std;
 
@@ -31,112 +34,7 @@ boost::optional<YAML::Node> maybeFind( string key, YAML::Node doc ) {
 }
 
 
-template<typename A, typename B>
-class either 
-{
-private: 
-  enum which_t { left, right } which;
-  boost::variant<A,B> content; 
-public: 
-  void put(A a) { which = left; content = a; } 
-  void put(B b) { which = right; content = b; }
 
-  bool isLeft( void ) { return (which == left); }
-  bool isRight( void ) { return (which == right); }
-
-  boost::optional<A> getLeft ( void ) 
-  {
-    if( which == left ) {
-      return boost::optional<A>(boost::get<A>(content)); 
-    }
-    else return boost::optional<A>(); 
-  }
-  boost::optional<B> getRight( void )
-  {
-    if( which == right ) 
-    {
-      return boost::optional<B>(boost::get<B>(content));
-    }
-    else return boost::optional<B>();
-  }
-};
-
-struct import
-{
-  string name;
-};
-
-struct meta_info_t
-{
-  string tag;
-  string description;
-  string comment; 
-  string reference; 
-};
-
-struct electron_eff_data_t
-{ 
-  string name;
-  meta_info_t meta_info; 
-}; 
-
-struct photon_eff_data_t
-{ 
-  string name;
-  meta_info_t meta_info; 
-}; 
-
-struct bjet_eff_data_t
-{ 
-  string name;
-  meta_info_t meta_info; 
-}; 
-
-struct muon_eff_data_t
-{
-  string name;
-  meta_info_t meta_info;
-};
-
-struct jet_eff_data_t
-{
-  string name;
-  meta_info_t meta_info;
-};
-
-struct tau_eff_data_t
-{
-  string name;
-  meta_info_t meta_info;
-};
-
-/*
-struct pt_threshold_eff_data_t
-{
-  string name;
-  meta_info_t meta_info;
-};
-*/
-
-struct object_description_t
-{ 
-  either <import,electron_eff_data_t> electron;
-  either <import,photon_eff_data_t> photon;
-  either <import,bjet_eff_data_t> bjet;
-  either <import,muon_eff_data_t> muon;
-  either <import,jet_eff_data_t> jet;
-  either <import,tau_eff_data_t> tau;
-};
-
-struct detector_description_t
-{ 
-  string name; 
-  string description; 
-  string reference; 
-  string comment;
-  string validation_info;
-  object_description_t object;
-}; 
 
 boost::optional<meta_info_t> 
 getMetaInfo( YAML::Node node ) 
