ea6d96e7c91b271c3101f342a42ca182e2bf2253
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-22 13:35:35 +0000

    implement either type

--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <boost/optional/optional.hpp>
+#include <boost/variant.hpp>
 #include "yaml-cpp/yaml.h"
 
 using namespace std;
@@ -29,6 +30,41 @@ boost::optional<YAML::Node> maybeFind( string key, YAML::Node doc ) {
   else return boost::optional<YAML::Node>();
 }
 
+struct import
+{
+  string name;
+};
+
+template<typename A, typename B>
+class either 
+{
+private: 
+  enum which_t { left, right } which;
+  boost::variant<A,B> content; 
+public: 
+  void put(A a) { which = left; content = a; } 
+  void put(B b) { which = right; content = b; }
+
+  bool isLeft( void ) { return (which == left); }
+  bool isRight( void ) { return (which == right); }
+
+  boost::optional<A> getLeft ( void ) 
+  {
+    if( which == left ) {
+      return boost::optional<A>(boost::get<A>(content)); 
+    }
+    else return boost::optional<A>(); 
+  }
+  boost::optional<B> getRight( void )
+  {
+    if( which == right ) 
+    {
+      return boost::optional<B>(boost::get<B>(content));
+    }
+    else return boost::optional<B>();
+  }
+};
+
 struct object_description
 { 
   string name;
@@ -48,15 +84,16 @@ boost::optional<object_description> getObjectDescription( YAML::Node node)
 {
   object_description od; 
 
-  boost::optional<string> s1 = maybeFind<string>("Name", node) ; 
-  if ( !s1.is_initialized() ) return boost::optional<object_description>();
+  boost::optional<string> s1;
+  if ( s1 = maybeFind<string>("Name", node) ) 
+  {
+    od.name = s1.get();
 
-  od.name = *(s1.get_ptr());
-
-  return boost::optional<object_description>(od);
+    return boost::optional<object_description>(od);
+  }
+  
+  return NULL;
 } 
-
-
 
 boost::optional<detector_description> getDetectorDescription( YAML::Node doc ) 
 {
@@ -105,7 +142,19 @@ void yamlparsetest( void ) {
   cout << "Reference is " << pdd -> reference << endl;
   cout << "Object.Name is " << pdd->object.name << endl;
 
-  
+  either<int,string> t ;
+  // t.put( "hello" );
+  t.put( 32 );
+  boost::optional<int> tl = t.getLeft();   
+
+  if( tl.is_initialized() ) {
+    cout << "tl : " << tl.get() << endl; 
+  } 
+  else {
+    cout << "tl is not initialized" << endl;
+  }
+
+  //<< ":" << t.isRight() << endl;  
 }
 
 
