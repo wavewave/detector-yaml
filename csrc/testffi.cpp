35e83e50d1aba25f3ea84be9f6e2dff2b3a359e5
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-22 15:05:14 +0000

    implementing parsing up to meta_info for all objects

--- csrc/testffi.cpp
+++ csrc/testffi.cpp
@@ -74,32 +74,68 @@ struct meta_info_t
   string reference; 
 };
 
-struct electron_eff_data 
+struct electron_eff_data_t
 { 
   string name;
   meta_info_t meta_info; 
 }; 
 
-struct muon_eff_data
+struct photon_eff_data_t
+{ 
+  string name;
+  meta_info_t meta_info; 
+}; 
+
+struct bjet_eff_data_t
+{ 
+  string name;
+  meta_info_t meta_info; 
+}; 
+
+struct muon_eff_data_t
 {
   string name;
   meta_info_t meta_info;
 };
 
-struct object_description
-{ 
-  either <import,electron_eff_data> electron;
-  either <import,muon_eff_data> muon;
+struct jet_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
 };
 
-struct detector_description 
+struct tau_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+
+/*
+struct pt_threshold_eff_data_t
+{
+  string name;
+  meta_info_t meta_info;
+};
+*/
+
+struct object_description_t
+{ 
+  either <import,electron_eff_data_t> electron;
+  either <import,photon_eff_data_t> photon;
+  either <import,bjet_eff_data_t> bjet;
+  either <import,muon_eff_data_t> muon;
+  either <import,jet_eff_data_t> jet;
+  either <import,tau_eff_data_t> tau;
+};
+
+struct detector_description_t
 { 
   string name; 
   string description; 
   string reference; 
   string comment;
   string validation_info;
-  object_description object;
+  object_description_t object;
 }; 
 
 boost::optional<meta_info_t> 
@@ -143,7 +179,7 @@ importOrDeal( boost::optional<T> getT (YAML::Node node), YAML::Node node )
   return NULL;
 }
 
-boost::optional<electron_eff_data> getElectronEffData( YAML::Node node ) 
+boost::optional<electron_eff_data_t> getElectronEffData( YAML::Node node ) 
 { 
   boost::optional<string> s1; 
   boost::optional<meta_info_t> minfo; 
@@ -151,15 +187,15 @@ boost::optional<electron_eff_data> getElectronEffData( YAML::Node node )
   if( (s1 = maybeFind<string>("Name", node) )
       && ( minfo = getMetaInfo(node) ) )
   { 
-    electron_eff_data eeff; 
+    electron_eff_data_t eeff; 
     eeff.name = s1.get(); 
     eeff.meta_info = minfo.get();
-    return boost::optional<electron_eff_data>(eeff); 
+    return boost::optional<electron_eff_data_t>(eeff); 
   }
   return NULL;
 }
 
-boost::optional<muon_eff_data> getMuonEffData( YAML::Node node ) 
+boost::optional<photon_eff_data_t> getPhotonEffData( YAML::Node node ) 
 {
   boost::optional<string> s1; 
   boost::optional<meta_info_t> minfo ;  
@@ -167,46 +203,124 @@ boost::optional<muon_eff_data> getMuonEffData( YAML::Node node )
   if( (s1 = maybeFind<string>("Name", node) )
       && ( minfo = getMetaInfo( node ) ) )
   { 
-    muon_eff_data mueff; 
+    photon_eff_data_t mueff; 
     mueff.name = s1.get(); 
     mueff.meta_info = minfo.get();
-    return boost::optional<muon_eff_data>(mueff); 
+    return boost::optional<photon_eff_data_t>(mueff); 
   }
   return NULL;
 }
 
-
-boost::optional<object_description> getObjectDescription( YAML::Node node)
+boost::optional<bjet_eff_data_t> getBJetEffData( YAML::Node node ) 
 {
-  object_description od; 
+  boost::optional<string> s1; 
+  boost::optional<meta_info_t> minfo ;  
 
-  boost::optional<YAML::Node> n1;
-  boost::optional<YAML::Node> n2;
-  boost::optional<either <import, electron_eff_data > > r1; 
-  boost::optional<either <import, muon_eff_data > > r2; 
+  if( (s1 = maybeFind<string>("Name", node) )
+      && ( minfo = getMetaInfo( node ) ) )
+  { 
+    bjet_eff_data_t mueff; 
+    mueff.name = s1.get(); 
+    mueff.meta_info = minfo.get();
+    return boost::optional<bjet_eff_data_t>(mueff); 
+  }
+  return NULL;
+}
 
+boost::optional<muon_eff_data_t> getMuonEffData( YAML::Node node ) 
+{
+  boost::optional<string> s1; 
+  boost::optional<meta_info_t> minfo ;  
+
+  if( (s1 = maybeFind<string>("Name", node) )
+      && ( minfo = getMetaInfo( node ) ) )
+  { 
+    muon_eff_data_t mueff; 
+    mueff.name = s1.get(); 
+    mueff.meta_info = minfo.get();
+    return boost::optional<muon_eff_data_t>(mueff); 
+  }
+  return NULL;
+}
+
+boost::optional<jet_eff_data_t> getJetEffData( YAML::Node node ) 
+{
+  boost::optional<string> s1; 
+  boost::optional<meta_info_t> minfo ;  
+
+  if( (s1 = maybeFind<string>("Name", node) )
+      && ( minfo = getMetaInfo( node ) ) )
+  { 
+    jet_eff_data_t mueff; 
+    mueff.name = s1.get(); 
+    mueff.meta_info = minfo.get();
+    return boost::optional<jet_eff_data_t>(mueff); 
+  }
+  return NULL;
+}
+
+boost::optional<tau_eff_data_t> getTauEffData( YAML::Node node ) 
+{
+  boost::optional<string> s1; 
+  boost::optional<meta_info_t> minfo ;  
+
+  if( (s1 = maybeFind<string>("Name", node) )
+      && ( minfo = getMetaInfo( node ) ) )
+  { 
+    tau_eff_data_t mueff; 
+    mueff.name = s1.get(); 
+    mueff.meta_info = minfo.get();
+    return boost::optional<tau_eff_data_t>(mueff); 
+  }
+  return NULL;
+}
+
+boost::optional<object_description_t> getObjectDescription( YAML::Node node)
+{
+  object_description_t od; 
+
+  boost::optional<YAML::Node> n1, n2, n3, n4, n5, n6, n7 ;
+  boost::optional<either <import, electron_eff_data_t > > r1; 
+  boost::optional<either <import, photon_eff_data_t > > r2; 
+  boost::optional<either <import, bjet_eff_data_t > > r3; 
+  boost::optional<either <import, muon_eff_data_t > > r4; 
+  boost::optional<either <import, jet_eff_data_t > > r5; 
+  boost::optional<either <import, tau_eff_data_t > > r6; 
+  // boost::optional<either <import, pt_threshold_eff_data_t > > r7; 
     
   if ( (n1 = maybeFind<YAML::Node>("Electron", node) )
-       && (n2 = maybeFind<YAML::Node>("Muon", node) )
-       && (r1 = importOrDeal( getElectronEffData, n1.get()) )
-       && (r2 = importOrDeal( getMuonEffData, n2.get()) )
+       && (r1 = importOrDeal( getElectronEffData, n1.get() ))
+       && (n2 = maybeFind<YAML::Node>("Photon", node) )
+       && (r2 = importOrDeal( getPhotonEffData, n2.get()) )
+       && (n3 = maybeFind<YAML::Node>("BJet", node) )
+       && (r3 = importOrDeal( getBJetEffData, n3.get()) )
+       && (n4 = maybeFind<YAML::Node>("Muon", node) )
+       && (r4 = importOrDeal( getMuonEffData, n4.get()) )
+       && (n5 = maybeFind<YAML::Node>("Jet", node) )
+       && (r5 = importOrDeal( getJetEffData, n5.get()) )
+       && (n6 = maybeFind<YAML::Node>("Tau", node) )
+       && (r6 = importOrDeal( getTauEffData, n6.get()) )
      )
   {
     od.electron = r1.get();
-    od.muon = r2.get();
-    return boost::optional<object_description>(od);
+    od.photon = r2.get();
+    od.bjet = r3.get();
+    od.muon = r4.get();
+    od.jet = r5.get();
+    od.tau = r6.get();
+    return boost::optional<object_description_t>(od);
   }
   
   return NULL;
 } 
 
-boost::optional<detector_description> getDetectorDescription( YAML::Node doc ) 
+boost::optional<detector_description_t> getDetectorDescription( YAML::Node doc ) 
 {
-  detector_description dd; 
+  detector_description_t dd; 
 
   boost::optional<string> s1,s2,s3,s4,s5; 
   boost::optional<YAML::Node> s6;
-  boost::optional<object_description> mo1;
+  boost::optional<object_description_t> mo1;
 
   if( (s1 = maybeFind<string>("Name", doc))
       && (s2 = maybeFind<string>("Description", doc)) 
@@ -224,28 +338,25 @@ boost::optional<detector_description> getDetectorDescription( YAML::Node doc )
     dd.comment = s4.get();
     dd.validation_info = s5.get();
     dd.object = mo1.get();
-    return boost::optional<detector_description>(dd);
+    return boost::optional<detector_description_t>(dd);
   }
   return NULL;
 }
-
-
-
 
 void yamlparsetest( void ) {
   std::ifstream input("temp/ATLAS2011.yaml");  
   YAML::Node doc = YAML::Load(input);
 
 
-  boost::optional<detector_description> mdd = getDetectorDescription(doc) ; 
+  boost::optional<detector_description_t> mdd = getDetectorDescription(doc) ; 
 
   if( !mdd.is_initialized() ) return; 
 
-  detector_description const *pdd = mdd.get_ptr(); 
-  cout << "Name is " << pdd->name << endl; 
-  cout << "Description is "<< pdd->description << endl;
-  cout << "Reference is " << pdd -> reference << endl;
-  either<import,electron_eff_data> e = pdd->object.electron; 
+  detector_description_t pdd = mdd.get(); 
+  cout << "Name is " << pdd.name << endl; 
+  cout << "Description is "<< pdd.description << endl;
+  cout << "Reference is " << pdd.reference << endl;
+  either<import,electron_eff_data_t> e = pdd.object.electron; 
   if( boost::optional<import> t1 = e.getLeft() )
   { 
     cout << "Object.Electron.Import is " << t1.get().name << endl;
@@ -253,8 +364,8 @@ void yamlparsetest( void ) {
   else cout << "Object.Electron parse failed" << endl;
     
 
-  either<import,muon_eff_data> mu = pdd->object.muon; 
-  if( boost::optional<muon_eff_data> t2 = mu.getRight() )
+  either<import,muon_eff_data_t> mu = pdd.object.muon; 
+  if( boost::optional<muon_eff_data_t> t2 = mu.getRight() )
   { 
     cout << "Object.Muon.Eff_data is " << t2.get().name << endl;
     cout << "Object.Muon.EffData.Description " << t2.get().meta_info.description << endl;
